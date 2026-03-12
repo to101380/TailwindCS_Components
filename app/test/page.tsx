6692801d1223e251bfaf1f946d9cce9deedf9854
to101380 <to101380@gmail.com>
2026-03-12 18:16:36 +0800

    add slide

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,18 +1,21 @@
 import Image from "next/image";
 import AppleNavbar from "@/components/navbar";
+import PeekCarousel from "@/components/peek-carousel";
 
+
+function HomePage() {
+    return (
+        <main className="min-h-screen bg-[#1b1b1f]">
+            <PeekCarousel />
+        </main>
+    );
+}
 
 
 function Page() {
     return (
-        <main className="min-h-[200vh] bg-white" style={{
-            backgroundImage: "url('https://dribs-drabs.com/wp-content/uploads/2025/12/00-8.jpg')",
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            backgroundRepeat: "no-repeat",
-        }}>
+        <main className="min-h-[200vh] bg-white">
             <AppleNavbar />
-
             <section className="px-6 pt-28 md:pt-32" >
                 <div className="mx-auto max-w-6xl">
                     <h1 className="text-4xl font-bold text-black">你的內容</h1>
@@ -27,6 +30,10 @@ function Page() {
 
 export default function Home() {
     return (
-        <Page />
+
+        <>
+            <Page />
+            <HomePage />
+        </>
     );
 }
